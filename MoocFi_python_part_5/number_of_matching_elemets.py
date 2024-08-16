f4c9089475a96953977cd56a439ab4ead5a1caac
Vishnu <69710928+vishnuodeyar@users.noreply.github.com>
2024-08-16 18:07:06 +0000

    Completed number_of_matching_elements exercise

---
+++ MoocFi_python_part_5/number_of_matching_elemets.py
@@ -0,0 +1,10 @@
+def count_matching_elements(my_matirx: list, element: int):
+  b = 0
+  for a in my_matirx:
+      for c in a:
+          if c == element:
+              b += 1
+  return b
+
+matrix = [[12,3,4], [34,56,3], [3,34,3]]
+print(count_matching_elements(matrix, 3))
