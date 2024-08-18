94da35825d2c7abc49b743900e1e4210b53d7db7
Vishnu <69710928+vishnuodeyar@users.noreply.github.com>
2024-08-18 14:15:46 +0000

    Completed suduko_check_row exercise

---
+++ MoocFi_python_part_5/suduko_check_row.py
@@ -0,0 +1,22 @@
+def row_correct(row: list, number: int):
+  for i in row:
+    for j in i:
+      if j == number:
+        return True
+        break
+    return False
+
+sudoku = [
+  [9, 0, 0, 0, 8, 0, 3, 0, 0],
+  [2, 0, 0, 2, 5, 0, 7, 0, 0],
+  [0, 2, 0, 3, 0, 0, 0, 0, 4],
+  [2, 9, 4, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 7, 3, 0, 5, 6, 0],
+  [7, 0, 5, 0, 6, 0, 4, 0, 0],
+  [0, 0, 7, 8, 0, 3, 9, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 0, 3],
+  [3, 0, 0, 0, 0, 0, 0, 0, 2]
+]
+
+print(row_correct(sudoku, 0))
+print(row_correct(sudoku, 1))
